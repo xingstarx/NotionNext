c1c998c94326f1abb9e2c64bb51ccd152f67a8cf
xingxing <823952375@qq.com>
2024-03-08 12:30:15 +0800

    文章详情页添加h1标签，首页的h2标签需要包裹文章图标

--- themes/next/components/ArticleDetail.js
+++ themes/next/components/ArticleDetail.js
@@ -57,11 +57,13 @@ export default function ArticleDetail(props) {
               )}
 
             {/* title */}
-            <div className=' text-center font-bold text-3xl text-black dark:text-white font-serif pt-6'>
-              {siteConfig('POST_TITLE_ICON') && (
-                <NotionIcon icon={post.pageIcon} />
-              )}
-              {post.title}
+            <div className=" text-center font-bold text-3xl text-black dark:text-white font-serif pt-6">
+              <h1>
+                {siteConfig('POST_TITLE_ICON') && (
+                  <NotionIcon icon={post.pageIcon} />
+                )}
+                {post.title}
+              </h1>
             </div>
 
             {/* meta */}
